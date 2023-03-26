c38a535bd2380ca3673b9dba8b29dd7d37981723
Vinicius Vieira <v.martins@aluno.ifsp.edu.br>
2023-03-26 20:05:29 -0300

    implementado script para processar datasets temporários

--- src/process_data.py
+++ src/process_data.py
@@ -3,39 +3,30 @@ import os
 
 absolute_path = os.path.dirname(__file__)
 
-files_to_read = ['vachina-tweets.csv', 'vacina china-tweets.csv', 'vacina cobaia-tweets.csv',
-                 'vacina sem comprovação-tweets.csv', 'vacina sem eficácia-tweets.csv']
+files_to_read = ['vacina sem eficácia-tweets.csv', 'vacina sem comprovação-tweets.csv', 'vacina cobaia-tweets.csv']
 
-df = pd.DataFrame()
+final_path = absolute_path + "\\vacinas-dataset.csv"
+
+df = pd.read_csv(final_path, index_col='id')
 
 for f in os.listdir(absolute_path + '\\temp'):
     print(f)
     if (f not in files_to_read):
         continue
-
-    temp_df = pd.read_csv(absolute_path + '\\temp\\' + f)
+    temp_path = absolute_path + '\\temp\\' + f
+    temp_df = pd.read_csv(temp_path, index_col='id')
     df = pd.concat([df, temp_df], axis=0)
 
-
-df[df['is_missinginfo'] == 1]
-df.shape
-df.head()
-
-# filter portuguese only messages
+# filter portuguese messages only
 df = df[df['lang'] == 'pt']
 df.shape
-
-# filter is_missing info not
-#df_final = df[df['is_missinginfo'].notnull()]
-# df_final.shape
-# df.head()
-
-df = df.sample(frac=1)
 
 print(df.shape)
 df.drop_duplicates(inplace=True)
 print(df.shape)
 
-df.to_csv('raw-vacinas-dataset.csv', index=False)
+df.head()
+df.to_csv('vacinas-dataset.csv', mode='w+', index=True)
 
+print(f"temp datasets merged into file {final_path}")
 print("DONE!")
