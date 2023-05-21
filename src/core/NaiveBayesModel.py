fb49834a026c129c7c4d4db32804476c6df418ca
Vinicius Vieira <viniciusmv91@gmail.com>
2023-05-21 18:19:35 -0300

    limpando código

--- src/core/NaiveBayesModel.py
+++ src/core/NaiveBayesModel.py
@@ -19,28 +19,9 @@ class NaiveBayesModel(PredictionModel):
 
     def train(self, x, y, test_size=0.25):
         super().train(x, y, test_size)
-        # # Preprocess tweets
-        # x_preprocessed = [self.preprocess_text(tweet) for tweet in x]
-
-        # # Split data between train and test
-        # x_train, x_test, y_train, y_test = train_test_split(x_preprocessed, y, test_size=test_size)
-
-        # # Set train size control fields
-        # self.total_tweets_train = len(x)
-        # self.fake_tweets_train = PredictionModel.get_fake_tweets_length(y_train)
-
-        # # Create TF-IDF vectorizer
-        # # self.vectorizer = TfidfVectorizer()
-        # self.vectorizer = TfidfVectorizer(strip_accents="ascii", ngram_range=(1, 2))
-        # x_tfidf = self.vectorizer.fit_transform(x_train)
-        # self.model.fit(x_tfidf, y_train)
-
-        # # Calculate model accuracy
-        # self.model_train_accuracy = self.model_score(x_train, y_train)
-        # self.model_test_accuracy = self.model_score(x_test, y_test)
 
     def show_info(self):
-        print("Gaussian Naive Bayes Model:")
+        print("Naive Bayes Model:")
         super().show_info()
 
     def model_score(self, x, y):
