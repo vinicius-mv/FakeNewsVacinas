4236e0d1e8f09895b02f5734aa999b5b5b97dd6c
Vinicius Vieira <viniciusmv91@gmail.com>
2023-08-20 22:37:10 -0300

    refactoring na funcionalidade de mergear dados, agora leitura completa da collection do mongo db e acrescentar data do csv selecionado

--- src/datamanagement/merge_datasets.py
+++ src/datamanagement/merge_datasets.py
@@ -4,18 +4,29 @@ sys.path.insert(0,"..")
 import pandas as pd
 import os
 
+from utils.MongoConnector import MongoConnector
+
 files_path = f"..\\datasets\\"
 
 # files already merged: 
 # 1. 'vacina sem eficácia-tweets.csv', 
 # 2. 'vacina sem comprovação-tweets.csv', 
 # 3. 'vacina cobaia-tweets.csv
-# 4. TODO: 'vacinas-tweets.csv
+# 4. 'vacinas-tweets.csv
 files_to_read = ['vacina-tweets.csv']
 
 final_path = files_path + "vacinas-dataset.csv"
 
-df = pd.read_csv(final_path, index_col="id")
+client = MongoConnector()
+client.initialize()
+
+collection = "tweets"
+query = {}
+cursor = client.find(collection, query)
+
+df = pd.DataFrame(cursor)
+df.head()
+df.set_index("_id", inplace=True)
 
 for f in os.listdir(files_path):
     if f not in files_to_read:
@@ -23,6 +34,7 @@ for f in os.listdir(files_path):
     print(f)
     current_file_path = files_path + f
     temp_df = pd.read_csv(current_file_path, index_col="id")
+    temp_df.rename(columns={'id': '_id'}, inplace=True)
     df = pd.concat([df, temp_df], axis=0)
 
 # filter portuguese messages only
